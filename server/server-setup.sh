77236d2f9ca97ad7a2b3374d90698a2eb6f5966a
Aaron D Borden <adborden@a14n.net>
2018-09-13 16:37:28 -0700

    Cleanup docker-compose for development
    - Don't copy the source in build, the src dirs are mapped to the container
    - Fix paths to app code in container (/app -> /usr/src/app)
    - Add node_modules to PATH to simplify commands
    - Use npm scripts where available
    - Map volumes to locations where docker writes files (otherwise you end up with
      root-owned files in your host directories) and add placeholders for the dirs
      so docker doesn't create them.
    - Don't expose postgres port which conflicts with postgres running on the host
    - Remove fs-intake-pa11y which doesn't seem to exist anymore

--- server/server-setup.sh
+++ server/server-setup.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 # Used by Docker
 
-cd /app
-
 sleep 10
 
-/node/node_modules/.bin/sequelize db:migrate
-
-/node/node_modules/.bin/sequelize db:seed:all
-
-/node/node_modules/.bin/nodemon --harmony_default_parameters app.es6
+npm run migrate
+npm run seed
+nodemon --harmony_default_parameters app.es6
