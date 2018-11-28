e9156adabbfca3c3f6e7025f3d685c472605f8dd
Himonas <12001001989940@FEDIDCARD.GOV>
2018-11-28 15:37:40 -0500

    Report query dates fix

--- frontend/src/app/trees/admin/report/report.component.ts
+++ frontend/src/app/trees/admin/report/report.component.ts
@@ -119,7 +119,7 @@ export class ReportComponent implements OnInit, AfterViewInit {
    * @returns forest by date
    */
   getForestDate(dateField) {
-    return moment.tz(this.form.get(dateField).value, this.forest.timezone).format('MM/DD/YYYY');
+    return moment.tz(this.form.get(dateField).value, "").format('MM/DD/YYYY');
   }
 
   /**
@@ -150,8 +150,8 @@ export class ReportComponent implements OnInit, AfterViewInit {
       this.setReportParameters();
       this.service.getAllByDateRange(
           this.forest.id,
-          moment.tz(this.form.get('dateTimeRange.startDateTime').value, this.forest.timezone).format('YYYY-MM-DD'),
-          moment.tz(this.form.get('dateTimeRange.endDateTime').value, this.forest.timezone).format('YYYY-MM-DD')
+          moment.tz(this.form.get('dateTimeRange.startDateTime').value, "").format('YYYY-MM-DD'),
+          moment.tz(this.form.get('dateTimeRange.endDateTime').value, "").format('YYYY-MM-DD')
         )
         .subscribe(results => {
             this.result = {
